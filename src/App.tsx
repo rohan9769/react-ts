d24f87905c2311f253f04196da27a1f350e1a4c6
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-31 22:02:35 -0700

    click event as props done

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Leo</Heading>
       </Oscar>
-      <Button handleClick={(event)=>{console.log('Button Clicked',event)}}></Button>
+      <Button handleClick={(event,id)=>{console.log('Button Clicked',event,id)}}></Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 type ButtonProps = {
-    handleClick:(event:React.MouseEvent<HTMLButtonElement>)=>void
+    handleClick:(event:React.MouseEvent<HTMLButtonElement>,id:number)=>void
 }
 export const Button = (props:ButtonProps) =>{
     return(
         <>
-            <button onClick={props.handleClick}>Click</button>
+            <button onClick={(event)=>props.handleClick(event,1)}>Click</button>
         </>
     )
 }

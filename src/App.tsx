a0b0927252c1c51acc554597c5f2f71ec84693b5
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-31 22:15:56 -0700

    input event handling props

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Status } from './components/Status';
 import { Heading } from './components/Heading';
 import Oscar from './components/Oscar';
 import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   const personName = {
@@ -38,6 +39,7 @@ function App() {
         <Heading>Oscar goes to Leo</Heading>
       </Oscar>
       <Button handleClick={(event,id)=>{console.log('Button Clicked',event,id)}}></Button>
+      <Input value='' handleChange={(event)=>console.log(event)}></Input>
     </div>
   );
 }
